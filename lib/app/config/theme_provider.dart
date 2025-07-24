926c5e7ef6acec289486108154af32877b80c3fb
Viswajith K S <viswajithks@ug.cusat.ac.in>
2025-07-24 14:43:36 +0530

    fix(theme): fix intial theme toggle inambiguity

--- lib/app/config/theme_provider.dart
+++ lib/app/config/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
@@ -6,7 +7,13 @@ final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
 });
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.system);
+  ThemeNotifier() : super(_getInitialTheme());
+
+  static ThemeMode _getInitialTheme() {
+    final brightness =
+        SchedulerBinding.instance.platformDispatcher.platformBrightness;
+    return brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+  }
 
   void toggleTheme() {
     state = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
